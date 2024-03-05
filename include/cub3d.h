a4f780156255ef44d79f9f6f9a5e70390755ac30
Neffi <114996380+Neffi42@users.noreply.github.com>
2024-03-05 17:36:23 +0100

    removed non-necessary declarations from cub3d.h

--- include/cub3d.h
+++ include/cub3d.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: averin <averin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: abasdere <abasdere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 11:15:26 by averin            #+#    #+#             */
-/*   Updated: 2024/03/05 14:32:46 by averin           ###   ########.fr       */
+/*   Updated: 2024/03/05 17:25:37 by abasdere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,8 @@
 
 # include "libft.h"
 # include "mlx.h"
+# include "utils.h"
 
 # include <stdio.h>
-
-# define WIDTH 600
-# define HEIGHT 800
-
-typedef struct s_window
-{
-	void	*mlx;
-	void	*ptr;
-}	t_window;
-
-int		create_window(t_window *window);
-void	delete_window(t_window *window);
 
 #endif
