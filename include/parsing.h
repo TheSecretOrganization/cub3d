1e8d807e1fb9b4653fb7e2041902dba6a18c9bad
averin <averin@student.42.fr>
2024-03-05 16:28:21 +0100

    add prototype for read_file

--- include/parsing.h
+++ include/parsing.h
@@ -6,13 +6,15 @@
 /*   By: averin <averin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 16:04:30 by averin            #+#    #+#             */
-/*   Updated: 2024/03/05 16:24:26 by averin           ###   ########.fr       */
+/*   Updated: 2024/03/05 16:28:11 by averin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSING_H
 # define PARSING_H
 
+# include "libft.h"
 
+t_list	*read_file(char *file);
 
 #endif
