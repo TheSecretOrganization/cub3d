457f28ee4abefc3d9305aac38d61d2d01f066034
averin <averin@student.42.fr>
2024-03-05 14:33:36 +0100

    call to create_window in main

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: averin <averin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 10:52:38 by averin            #+#    #+#             */
-/*   Updated: 2024/03/04 13:06:18 by averin           ###   ########.fr       */
+/*   Updated: 2024/03/05 14:33:16 by averin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,11 @@
 
 int	main(int argc, char const *argv[])
 {
+	t_window	window;
+
 	(void)argc;
 	(void)argv;
+	if (create_window(&window) == -1)
+		return (printf("error: cannot open window\n"), 1);
 	return (0);
 }
