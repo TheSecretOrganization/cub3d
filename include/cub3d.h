4fe8658fa8a3d2937ee92dbc70fb60c20eb330a4
averin <averin@student.42.fr>
2024-03-05 14:27:28 +0100

    add s_window structure

--- include/cub3d.h
+++ include/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: averin <averin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 11:15:26 by averin            #+#    #+#             */
-/*   Updated: 2024/03/04 11:16:27 by averin           ###   ########.fr       */
+/*   Updated: 2024/03/05 14:24:15 by averin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,11 @@
 
 # include "libft.h"
 # include "mlx.h"
+
+typedef struct s_window
+{
+	void	*mlx;
+	void	*ptr;
+}	t_window;
 
 #endif
