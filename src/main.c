8c26e69165d6b97621fb16d87a103712eb437917
Neffi <114996380+Neffi42@users.noreply.github.com>
2024-03-06 11:07:25 +0100

    Created usage check

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: abasdere <abasdere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 10:52:38 by averin            #+#    #+#             */
-/*   Updated: 2024/03/05 17:16:34 by abasdere         ###   ########.fr       */
+/*   Updated: 2024/03/06 11:06:54 by abasdere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,16 @@
 #include "collector.h"
 #include "window.h"
 
+#define USAGE_ERROR "Usage: ./cub3d <file.cub>"
+
 int	main(int argc, char const *argv[])
 {
 	t_window	window;
 	t_collector	*collector;
 
-	(void)argc;
 	(void)argv;
+	if (argc != 2)
+		error(USAGE_ERROR);
 	collector = init_collector();
 	create_window(&window, collector);
 	return (free_collector(collector), 0);
