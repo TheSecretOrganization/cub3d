2935d9a40a46488076e67a1fc9aa4aeed72aaf31
Neffi <114996380+Neffi42@users.noreply.github.com>
2024-03-06 12:45:29 +0100

    created check_suffix and added close statement in read_file

--- src/read_file.c
+++ src/read_file.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   read_file.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: averin <averin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: abasdere <abasdere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 16:03:32 by averin            #+#    #+#             */
-/*   Updated: 2024/03/06 10:48:07 by averin           ###   ########.fr       */
+/*   Updated: 2024/03/06 12:44:45 by abasdere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,21 @@
 
 #define MALLOC_ERROR "Malloc failed"
 #define OPEN_ERROR "Cannot open file"
+#define SUFFIX_ERROR "Invalid suffix, use a <file.cub>"
 
 void	clear_file(void *v)
 {
 	ft_lstclear((t_list **) &v, &free);
+}
+
+static void	check_suffix(const char *file, t_collector *collector)
+{
+	size_t	len;
+
+	len = ft_strlen(file);
+	if (len < 4 || file[len - 4] != '.' || file[len - 3] != 'c'\
+		|| file[len - 2] != 'u' || file[len - 1] != 'b')
+		cerror(SUFFIX_ERROR, collector);
 }
 
 t_list	*read_file(const char *file, t_collector *collector)
@@ -30,7 +41,7 @@ t_list	*read_file(const char *file, t_collector *collector)
 	int		fd;
 
 	lst = NULL;
-	fd = open(file, O_RDONLY);
+	(check_suffix(file, collector), fd = open(file, O_RDONLY));
 	if (fd == -1)
 		cerror(OPEN_ERROR, collector);
 	while (oget_next_line(fd, &line))
@@ -38,13 +49,14 @@ t_list	*read_file(const char *file, t_collector *collector)
 		trimmed = ft_strtrim(line, "\n");
 		free(line);
 		if (!trimmed)
-			(ft_lstclear(&lst, &free), cerror(MALLOC_ERROR, collector));
+			(ft_lstclear(&lst, &free), close(fd),
+				cerror(MALLOC_ERROR, collector));
 		new_element = ft_lstnew(trimmed);
 		if (!new_element)
-			(ft_lstclear(&lst, &free), free(trimmed),
+			(ft_lstclear(&lst, &free), free(trimmed), close(fd),
 				cerror(MALLOC_ERROR, collector));
 		ft_lstadd_back(&lst, new_element);
 	}
-	add_collector(collector, lst, &clear_file);
+	(close(fd), add_collector(collector, lst, &clear_file));
 	return (lst);
 }
