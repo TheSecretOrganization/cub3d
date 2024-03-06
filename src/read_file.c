47bfa2bb729f517a62bc5b109850c054d9d66bbc
Neffi <114996380+Neffi42@users.noreply.github.com>
2024-03-06 16:15:16 +0100

    updated check_suffix

--- src/read_file.c
+++ src/read_file.c
@@ -6,7 +6,7 @@
 /*   By: abasdere <abasdere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 16:03:32 by averin            #+#    #+#             */
-/*   Updated: 2024/03/06 12:44:45 by abasdere         ###   ########.fr       */
+/*   Updated: 2024/03/06 16:14:35 by abasdere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,24 @@
 
 #define MALLOC_ERROR "Malloc failed"
 #define OPEN_ERROR "Cannot open file"
-#define SUFFIX_ERROR "Invalid suffix, use a <file.cub>"
+#define SUFFIX_ERROR "Invalid suffix, use a <map.cub>"
 
 void	clear_file(void *v)
 {
 	ft_lstclear((t_list **) &v, &free);
 }
 
-static void	check_suffix(const char *file, t_collector *collector)
+static void	check_suffix(const char *f, const char *s, t_collector *col)
 {
 	size_t	len;
 
-	len = ft_strlen(file);
-	if (len < 4 || file[len - 4] != '.' || file[len - 3] != 'c'\
-		|| file[len - 2] != 'u' || file[len - 1] != 'b')
-		cerror(SUFFIX_ERROR, collector);
+	len = ft_strlen(f);
+	if (len < 4
+		|| f[len - 4] != s[0]
+		|| f[len - 3] != s[1]
+		|| f[len - 2] != s[2]
+		|| f[len - 1] != s[3])
+		cerror(SUFFIX_ERROR, col);
 }
 
 t_list	*read_file(const char *file, t_collector *collector)
@@ -41,7 +44,7 @@ t_list	*read_file(const char *file, t_collector *collector)
 	int		fd;
 
 	lst = NULL;
-	(check_suffix(file, collector), fd = open(file, O_RDONLY));
+	(check_suffix(file, ".cub", collector), fd = open(file, O_RDONLY));
 	if (fd == -1)
 		cerror(OPEN_ERROR, collector);
 	while (oget_next_line(fd, &line))
