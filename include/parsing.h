c8b766a4f42561338be28430504b8fbf1063e90c
Neffi <114996380+Neffi42@users.noreply.github.com>
2024-03-07 11:43:52 +0100

    created t_pmap structure

--- include/parsing.h
+++ include/parsing.h
@@ -6,7 +6,7 @@
 /*   By: abasdere <abasdere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 16:04:30 by averin            #+#    #+#             */
-/*   Updated: 2024/03/07 10:43:39 by abasdere         ###   ########.fr       */
+/*   Updated: 2024/03/07 11:37:10 by abasdere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,12 @@
 
 # include <fcntl.h>
 # include <sys/stat.h>
+
+typedef struct s_pmap
+{
+	t_list	*map;
+	size_t	size;
+}	t_pmap;
 
 t_list	*read_file(const char *file, t_collector *collector);
 void	parse_file(t_map *map, const char *argv, t_collector *collector);
