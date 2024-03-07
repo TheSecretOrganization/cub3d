a9eac9bf427feee6951a736dde06f0a915f1be43
Neffi <114996380+Neffi42@users.noreply.github.com>
2024-03-07 11:09:19 +0100

    put check_suffix in static

--- include/parsing.h
+++ include/parsing.h
@@ -6,7 +6,7 @@
 /*   By: abasdere <abasdere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 16:04:30 by averin            #+#    #+#             */
-/*   Updated: 2024/03/06 15:23:02 by abasdere         ###   ########.fr       */
+/*   Updated: 2024/03/07 10:43:39 by abasdere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,6 @@
 # include <fcntl.h>
 # include <sys/stat.h>
 
-void	check_suffix(const char *file, const char *suffix, t_collector *col);
 t_list	*read_file(const char *file, t_collector *collector);
 void	parse_file(t_map *map, const char *argv, t_collector *collector);
 
--- src/read_file.c
+++ src/read_file.c
@@ -6,7 +6,7 @@
 /*   By: abasdere <abasdere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 16:03:32 by averin            #+#    #+#             */
-/*   Updated: 2024/03/06 16:14:35 by abasdere         ###   ########.fr       */
+/*   Updated: 2024/03/07 10:44:29 by abasdere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,16 +22,16 @@ void	clear_file(void *v)
 	ft_lstclear((t_list **) &v, &free);
 }
 
-static void	check_suffix(const char *f, const char *s, t_collector *col)
+static void	check_suffix(const char *f, t_collector *col)
 {
 	size_t	len;
 
 	len = ft_strlen(f);
 	if (len < 4
-		|| f[len - 4] != s[0]
-		|| f[len - 3] != s[1]
-		|| f[len - 2] != s[2]
-		|| f[len - 1] != s[3])
+		|| f[len - 4] != '.'
+		|| f[len - 3] != 'c'
+		|| f[len - 2] != 'u'
+		|| f[len - 1] != 'b')
 		cerror(SUFFIX_ERROR, col);
 }
 
@@ -44,7 +44,7 @@ t_list	*read_file(const char *file, t_collector *collector)
 	int		fd;
 
 	lst = NULL;
-	(check_suffix(file, ".cub", collector), fd = open(file, O_RDONLY));
+	(check_suffix(file, collector), fd = open(file, O_RDONLY));
 	if (fd == -1)
 		cerror(OPEN_ERROR, collector);
 	while (oget_next_line(fd, &line))
