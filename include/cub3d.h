8f7201ed76a25fe026310f0dbee12833d1fa5bd6
Neffi <114996380+Neffi42@users.noreply.github.com>
2024-03-11 10:54:37 +0100

    updated t_vector to float

--- include/cub3d.h
+++ include/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: abasdere <abasdere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 11:15:26 by averin            #+#    #+#             */
-/*   Updated: 2024/03/07 13:02:05 by abasdere         ###   ########.fr       */
+/*   Updated: 2024/03/11 10:54:01 by abasdere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,8 @@
 
 typedef struct s_vector
 {
-	double	x;
-	double	y;
+	float	x;
+	float	y;
 }	t_vector;
 
 typedef struct s_player
