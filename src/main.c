f49ea3ef88059393015d6df4d3499c5e1e173a78
averin <averin@student.42.fr>
2024-03-12 15:39:45 +0100

    prototype for raycast function

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: averin <averin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 10:52:38 by averin            #+#    #+#             */
-/*   Updated: 2024/03/11 12:56:26 by averin           ###   ########.fr       */
+/*   Updated: 2024/03/12 15:39:22 by averin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,83 @@
 #include "collector.h"
 #include "parsing.h"
 #include "window.h"
+#include <math.h>
 
 #define USAGE_ERROR "Usage: ./cub3d <file.cub>"
+
+float	raycast(t_vector *position, t_vector *direction, t_map *map)
+{
+	int hit = 0;
+	int face;
+	t_vector step = {0, 0};
+	t_vector side = {0, 0};
+	int map_pos[2] = {(int) position->x, (int) position->y};
+	t_vector delta = {
+		direction->x == 0 ? 1e30 : fabs(1 / direction->x),
+		direction->y == 0 ? 1e30 : fabs(1 / direction->y)
+	};
+	float	distance = -1;
+
+	// printf("starting map pos: %d %d\n", map_pos[0], map_pos[1]);
+	if (direction->x < 0)
+	{
+		step.x = -1;
+		side.x = (position->x - (int) position->x) * delta.x;
+	}
+	else
+	{
+		step.x = 1;
+		side.x = (position->x + 1 - (int) position->x) * delta.x;
+	}
+	if (direction->y >= 0)
+	{
+		step.y = -1;
+		side.y = (position->y - (int) position->y) * delta.y;
+	}
+	else
+	{
+		step.y = 1;
+		side.y = (position->y + 1 - (int) position->y) * delta.y;
+	}
+
+	printf("step: %f %f\nside %f %f\ndelta: %f %f\n", step.x, step.y, side.x, side.y, delta.x, delta.y);
+
+	while (hit == 0)
+	{
+		if (side.x < side.y)
+		{
+			side.x += delta.x;
+			map_pos[0] += step.x;
+			face = 0;
+		}
+		else
+		{
+			side.y += delta.y;
+			map_pos[1] += step.y;
+			face = 1;
+		}
+		printf("side: %f %f\tmap %d %d\n", side.x, side.y, map_pos[0], map_pos[1]);
+		if (map->content[map_pos[1]][map_pos[0]] == '1') hit = 1;
+	}
+	printf("WALL\n");
+	printf("step: %f %f\nside %f %f\ndelta: %f %f\n", step.x, step.y, side.x, side.y, delta.x, delta.y);
+	if (face == 0)
+	{
+		if (step.x < 0)
+			distance = side.x - delta.x;
+		else
+			distance = side.x - delta.x * 2;
+	}
+	else
+	{
+		if (step.y < 0)
+			distance = side.y - delta.y;
+		else
+			distance = side.y - delta.y * 2;
+	}
+	printf("distance: %f\n", distance);
+	return (distance);
+}
 
 int	main(int argc, char const *argv[])
 {
