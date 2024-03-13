45a52a8f88781f3763de034ea5d41782b14d0a48
averin <averin@student.42.fr>
2024-03-13 11:17:39 +0100

    removed unnecessary pointer in raycast

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abasdere <abasdere@student.42.fr>          +#+  +:+       +#+        */
+/*   By: averin <averin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 10:52:38 by averin            #+#    #+#             */
-/*   Updated: 2024/03/13 10:05:01 by abasdere         ###   ########.fr       */
+/*   Updated: 2024/03/13 11:17:17 by averin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,38 +18,38 @@
 
 #define USAGE_ERROR "Usage: ./cub3d <file.cub>"
 
-float	raycast(t_vector *position, t_vector *direction, t_map *map)
+float	raycast(t_vector position, t_vector direction, t_map map)
 {
 	int hit = 0;
 	int face;
 	t_vector step = {0, 0};
 	t_vector side = {0, 0};
-	int map_pos[2] = {(int) position->x, (int) position->y};
+	int map_pos[2] = {(int) position.x, (int) position.y};
 	t_vector delta = {
-		direction->x == 0 ? 1e30 : fabs(1 / direction->x),
-		direction->y == 0 ? 1e30 : fabs(1 / direction->y)
+		direction.x == 0 ? 1e30 : fabs(1 / direction.x),
+		direction.y == 0 ? 1e30 : fabs(1 / direction.y)
 	};
 	float	distance = -1;
 
-	if (direction->x < 0)
+	if (direction.x < 0)
 	{
 		step.x = -1;
-		side.x = (position->x - (int) position->x) * delta.x;
+		side.x = (position.x - (int) position.x) * delta.x;
 	}
 	else
 	{
 		step.x = 1;
-		side.x = (position->x + 1 - (int) position->x) * delta.x;
+		side.x = (position.x + 1 - (int) position.x) * delta.x;
 	}
-	if (direction->y >= 0)
+	if (direction.y >= 0)
 	{
 		step.y = -1;
-		side.y = (position->y - (int) position->y) * delta.y;
+		side.y = (position.y - (int) position.y) * delta.y;
 	}
 	else
 	{
 		step.y = 1;
-		side.y = (position->y + 1 - (int) position->y) * delta.y;
+		side.y = (position.y + 1 - (int) position.y) * delta.y;
 	}
 	while (hit == 0)
 	{
@@ -65,7 +65,7 @@ float	raycast(t_vector *position, t_vector *direction, t_map *map)
 			map_pos[1] += step.y;
 			face = 1;
 		}
-		if (map->content[map_pos[1]][map_pos[0]] == '1') hit = 1;
+		if (map.content[map_pos[1]][map_pos[0]] == '1') hit = 1;
 	}
 	if (face == 0)
 	{
