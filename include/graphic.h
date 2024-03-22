46e73faf786edd55938a792b0ddaf18bbef81623
Neffi42 <114996380+Neffi42@users.noreply.github.com>
2024-03-22 11:28:33 +0100

    deleted useless includes

--- include/graphic.h
+++ include/graphic.h
@@ -6,15 +6,12 @@
 /*   By: abasdere <abasdere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 11:15:07 by abasdere          #+#    #+#             */
-/*   Updated: 2024/03/22 10:33:57 by abasdere         ###   ########.fr       */
+/*   Updated: 2024/03/22 11:28:02 by abasdere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GRAPHIC_H
 # define GRAPHIC_H
-
-# include "libft.h"
-# include "collector.h"
 
 typedef struct s_color
 {
