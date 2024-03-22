be7cf8643f047dbcfffc9fb1feaf39482f9d5313
Neffi42 <114996380+Neffi42@users.noreply.github.com>
2024-03-22 11:04:57 +0100

    created t_image

--- include/window.h
+++ include/window.h
@@ -6,7 +6,7 @@
 /*   By: abasdere <abasdere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 17:14:09 by abasdere          #+#    #+#             */
-/*   Updated: 2024/03/22 10:57:20 by abasdere         ###   ########.fr       */
+/*   Updated: 2024/03/22 11:04:31 by abasdere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,15 @@
 
 # define WIDTH 800
 # define HEIGHT 600
+
+typedef struct t_img
+{
+	void	*ptr;
+	char	*content;
+	int		bpp;
+	int		size_line;
+	int		endian;
+}	t_img;
 
 typedef struct s_window
 {
