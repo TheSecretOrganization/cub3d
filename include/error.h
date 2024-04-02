fbc7eabd94c38a6dcc7e52a76cdbc18d0673967b
averin <averin@student.42.fr>
2024-04-02 14:18:37 +0200

    add D to valid chars in map

--- include/error.h
+++ include/error.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   error.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abasdere <abasdere@student.42.fr>          +#+  +:+       +#+        */
+/*   By: averin <averin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/21 11:22:41 by abasdere          #+#    #+#             */
-/*   Updated: 2024/03/21 13:34:31 by abasdere         ###   ########.fr       */
+/*   Updated: 2024/04/02 14:16:02 by averin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ NO, SO, WE and EA"
 # define MAN_COLOR_ERROR "The map doesn't contain all required colors: F and C"
 # define OPEN_ERROR "Cannot open file"
 # define SUFFIX_ERROR "Invalid suffix, use a <map.cub>"
-# define VALID_CHAR "10NSEW "
+# define VALID_CHAR "10NSEWD "
 # define PLAYER_VIEW "NSEW"
 # define CHAR_ERROR "Invalid char found in the map"
 # define NO_PLAYER "No starting position was found"
