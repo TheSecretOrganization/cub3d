b87bbcb78470b8fb92f1ec386f943110d3ca87a6
Neffi42 <114996380+Neffi42@users.noreply.github.com>
2024-04-10 18:40:18 +0200

    sprite collision ok

--- src/move.c
+++ src/move.c
@@ -3,59 +3,83 @@
 /*                                                        :::      ::::::::   */
 /*   move.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: averin <averin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: abasdere <abasdere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/29 08:57:21 by abasdere          #+#    #+#             */
-/*   Updated: 2024/04/09 16:52:09 by averin           ###   ########.fr       */
+/*   Updated: 2024/04/10 18:39:15 by abasdere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-static int	check_collision(char c, int x, int y, t_map map)
+#define RADIUS 0.3
+
+static int	check_sprite_collision(t_vector player_pos, t_vector sprite_pos)
 {
-	if (c == '1' || c == '\0')
+	float	dx;
+	float	dy;
+	float	d_sqrd;
+	float	col_d_sqrd;
+
+	dx = player_pos.x - sprite_pos.x;
+	dy = player_pos.y - sprite_pos.y;
+	d_sqrd = dx * dx + dy * dy;
+	col_d_sqrd = RADIUS * RADIUS;
+	return (d_sqrd < col_d_sqrd);
+}
+
+static int	check_collision(float x, float y, t_map map, int side)
+{
+	t_sprite	*s;
+
+	if (map.content[(int)y][(int)x] == '1'
+		|| map.content[(int)y][(int)x] == '\0')
 		return (1);
-	if (c == 'D' && !is_door_open(x, y, map))
+	if (map.content[(int)y][(int)x] == 'D' && !is_door_open(x, y, map))
 		return (1);
+	s = map.graphic.sprite;
+	while (s)
+	{
+		if (!side && check_sprite_collision((t_vector){x, y}, s->pos))
+			return (1);
+		else if (side && check_sprite_collision((t_vector){y, x}, s->pos))
+			return (1);
+		s = s->next;
+	}
 	return (0);
 }
 
 void	forward(float step, t_player *player, t_map map)
 {
-	size_t	x;
-	size_t	y;
+	float	x;
+	float	y;
 
-	x = (size_t)(player->pos.x + step * player->direction.x);
-	y = (size_t)(player->pos.y + step * player->direction.y);
-	if (x > map.width || y > map.heigh)
+	x = player->pos.x + step * player->direction.x;
+	y = player->pos.y + step * player->direction.y;
+	if ((size_t)x > map.width || (size_t)y > map.heigh)
 		return ;
-	if (!check_collision(map.content[(size_t)player->pos.y][x],
-		x, player->pos.y, map))
-		player->pos.x += step * player->direction.x;
-	if (!check_collision(map.content[y][(size_t)player->pos.x],
-		player->pos.x, y, map))
-		player->pos.y += step * player->direction.y;
+	if (!check_collision(x, player->pos.y, map, 0))
+		player->pos.x = x;
+	if (!check_collision(player->pos.x, y, map, 0))
+		player->pos.y = y;
 }
 
 void	side(int is_left, t_player *player, t_map map)
 {
 	t_vector	perpendicular;
-	size_t		x;
-	size_t		y;
+	float		x;
+	float		y;
 
 	if (is_left)
 		perpendicular = (t_vector){-player->direction.y, player->direction.x};
 	else
 		perpendicular = (t_vector){player->direction.y, -player->direction.x};
-	x = (size_t)(player->pos.x + STEP * perpendicular.x);
-	y = (size_t)(player->pos.y + STEP * perpendicular.y);
-	if (!check_collision(map.content[(size_t)player->pos.y][x],
-		player->pos.y, x, map))
-		player->pos.x += STEP * perpendicular.x;
-	if (!check_collision(map.content[y][(size_t)player->pos.x],
-		y, player->pos.x, map))
-		player->pos.y += STEP * perpendicular.y;
+	x = player->pos.x + STEP * perpendicular.x;
+	y = player->pos.y + STEP * perpendicular.y;
+	if (!check_collision(player->pos.y, x, map, 1))
+		player->pos.x = x;
+	if (!check_collision(y, player->pos.x, map, 1))
+		player->pos.y = y;
 }
 
 void	rotate(float v, t_player *player)
